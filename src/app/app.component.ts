e209eaa90c7c76f897dce13192f30be572abac82
OsmelMdz <mdz99h@gmail.com>
2021-08-10 13:31:19 -0500

    Mi Primer commit en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html'
+  //template: '<span>Osmel Mendez</span>'
 })
 export class AppComponent {
-  title = 'bases';
+  
 }
+
